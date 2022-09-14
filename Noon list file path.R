ed8024029a66eb992195eb2d8b6b77bb88cbc75a
Resendez <sgresendez@miners.utep.edu>
2022-09-14 12:31:29 -0500

    Adding working directory

--- Noon list file path.R
+++ Noon list file path.R
@@ -1,3 +1,6 @@
 #code to create noon-list with file path only
 #September 14, 2022 
 #written by Suellen Resendez and Dr. Marguerite Mauritz
+
+#Set working directory to read lists
+setwd("Z:/Research Data/Desert/Jornada/Bahada/Phenocam/TwrPhenocam/lists")
